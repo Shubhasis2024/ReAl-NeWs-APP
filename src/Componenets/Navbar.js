78c6b6e9ca08f60ea2d70e106e6974257c2d3194
Shubhasis Ghosh <149863773+Shubhasis2024@users.noreply.github.com>
2024-11-06 13:41:03 +0530

    Update Navbar.js

--- src/Componenets/Navbar.js
+++ src/Componenets/Navbar.js
@@ -7,7 +7,7 @@ export class Navbar extends Component {
   render() {
     return (
       <div>
-        <nav className="navbar navbar-expand-lg bg-body-tertiary">
+        <nav className="navbar navbar-dark bg-dark">
   <div className="container-fluid">
     <a className="navbar-brand" href="/">NewsHub</a>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -16,10 +16,28 @@ export class Navbar extends Component {
     <div className="collapse navbar-collapse" id="navbarSupportedContent">
       <ul className="navbar-nav me-auto mb-2 mb-lg-0">
         <li className="nav-item">
-          <a className="nav-link active" aria-current="page" href="/.Home">Home</a>
+          <a className="nav-link " aria-current="page" href="/.Home">Home</a>
         </li>
         <li className="nav-item">
-          <a className="nav-link" href="/.othernews">Other News</a>
+          <a className="nav-link" href="/.othernews">Scince&Technologys</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="/.othernews">Geopplitics</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="/.othernews">Busniess</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="/.othernews">Sports</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="/.othernews">Health</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="/.othernews">Fashion</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="/.othernews">Media</a>
         </li>
         </ul>
       
