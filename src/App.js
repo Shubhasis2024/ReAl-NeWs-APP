486bbdebfed5269ded2e45bb97dfb1f7d8b8112e
Shubhasis Ghosh <149863773+Shubhasis2024@users.noreply.github.com>
2024-11-07 14:21:46 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
-
 import './App.css';
 
 
 import React, { Component } from 'react'
 import Navbar from './Componenets/Navbar';
-import News from './Componenets/News';
+import News from'./Componenets/News';
 
 export default class App extends Component {
+  apikey=process.env.REACT_APP_NEWS_API
   render() {
+   
     return (
       <div>
-        <Navbar></Navbar>
-       <News></News>
+       <Navbar></Navbar>
+       <News apikey={this.apikey}></News>
       </div>
     )
   }
 }
-
